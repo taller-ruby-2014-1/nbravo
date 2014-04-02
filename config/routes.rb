6fe2ecc22a0bb4ca206d0b94fdf2dab96dde3466
Alumno <alumno@localhost.localdomain>
2014-04-02 16:25:22 -0300

    Hoy fue un lindo día, aprendí mucho

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Blog::Application.routes.draw do
+  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,7 +14,8 @@ Blog::Application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  resources :posts
+  
   # Example resource route with options:
   #   resources :products do
   #     member do
